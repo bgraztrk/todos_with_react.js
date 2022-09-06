635e24f54362fa3f7661224efd975124757eee5f
bgraztrk <77465499+bgraztrk@users.noreply.github.com>
2022-09-06 12:37:50 +0300

    Add files via upload

---
+++ components/TodoList.js
@@ -0,0 +1,49 @@
+import {useState, useEffect} from 'react';
+import Header from "./Header";
+import  Main  from "./Main";
+import Footer from "./Footer";
+
+
+function TodoList() {
+
+    const [todo, setTodo] = useState([]);
+    const [status, setStatus]= useState('');
+    const [filteredTodos, setFilteredTodos]= useState([]);
+
+    useEffect(() => {
+      const filterHandler = () => {
+      switch(status){
+        case 'completed':
+          setFilteredTodos(todo.filter(item => item.completed === true))
+          break;
+        case 'active':
+          setFilteredTodos(todo.filter(item => item.completed === false))
+          break;
+        default:
+          setFilteredTodos(todo)
+          break;
+      }
+    }
+  
+      filterHandler()
+    }, [todo, status])
+
+  return (
+    <div>
+        <div className="todoapp" >
+          
+        <Header addTodo={setTodo} todo={todo}/>
+        <Main filteredTodos={filteredTodos} todol={todo} deleteTodo={setTodo} status={status}/>
+        <Footer todoCount={todo} setTodo={setTodo} status={status} setStatus={setStatus} />
+      </div>
+
+      <footer className="info">
+	      <p>Click to edit a todo</p>
+	      <p>Created by <a href="https://github.com/Bgraztrk">Kemal Buğra Öztürk</a></p>
+	      <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+      </footer>
+    </div>
+  )
+}
+
+export default TodoList;
